aa5583e370717f778c4b659f37cd162332ce55e3
marrs21 <ashledep@oregonstate.edu>
2024-08-10 18:12:57 -0700

    VIM fix
    f u jacob

--- Card.cpp
+++ Card.cpp
@@ -1,37 +1,35 @@
 #include "Card.h"
-  2 
-  3 Card::Card(Ranks RANK, Suits SUIT)
-  4 {
-  5         Rank(RANK),
-  6         Suit(SUIT)
-  7 
-  8 }
-  9 
- 10 Card::~Card()
- 11 {
- 12         //Nothing here
- 13 }
- 14 
- 15 Card::Ranks Card::getRank()
- 16 {
- 17         return Rank;
- 18 
- 19 }
- 20 
- 21 card::Suits Card::getSuits()
- 22 {
- 23         return Suit;
- 24 
- 25 }
- 26 
- 27 int Card::getFaceValue()
- 28 {
- 29         if(Rank<=TEN){
- 30                 return static_cast<int>(Rank);
- 31         }
- 32         if(Rank <= KING){
- 33                 return 10;
- 34 
- 35         return 11;
- 36 
- 37 }
+
+Card::Card(Ranks RANK, Suits SUIT)
+{
+  Rank(RANK),
+  Suit(SUIT)
+
+}
+
+Card::~Card()
+{
+  //Nothing here
+}
+ 
+Card::Ranks Card::getRank()
+{
+  return Rank;
+}
+
+card::Suits Card::getSuits()
+{
+  return Suit;
+
+}
+
+int Card::getFaceValue()
+{
+  if(Rank<=TEN){
+    return static_cast<int>(Rank);
+  }
+  if(Rank <= KING && Rank >=10){
+    return 10;
+  }
+  return 11;
+}
--- Card.h
+++ Card.h
@@ -1,40 +1,39 @@
 class Card
-  2 {
-  3 public:
-  4         enum Suits
-  5         {
-  6                 HEARTS = 3,
-  7                 DIAMONDS,
-  8                 CLUBS,
-  9                 SPADES
- 10         };
- 11 
- 12         enum Ranks
- 13         {
- 14                 TWO = 2,
- 15                 THREE,
- 16                 FOUR,
- 17                 FIVE,
- 18                 SIX,
- 19                 SEVEN,
- 20                 EIGHT,
- 21                 NINE,
- 22                 TEN,
- 23                 JACK,
- 24                 QUEEN,
- 25                 KING,
- 26                 ACE
- 27         };
- 28 
- 29         Card(Ranks RANK, Suits SUIT);
- 30         virtual ~Card();
- 31 
- 32         Ranks getRank();
- 33         Suits  getSuit();
- 34 
- 35         virtual int getFaceValue();
- 36 private:
- 37         Ranks RANK;
- 38         Suits SUIT;
- 39 
- 40 };
+{
+    public:
+        enum Suits
+        {
+            HEARTS = 3,
+            DIAMONDS,
+            CLUBS,
+            SPADES
+        };
+        enum Ranks
+        {
+            TWO = 2,
+            THREE,
+            FOUR,
+            FIVE,
+            SIX,
+            SEVEN,
+            EIGHT,
+            NINE,
+            TEN,
+            JACK,
+            QUEEN,
+            KING,
+            ACE
+        };
+
+        Card(Ranks RANK, Suits SUIT);
+        virtual ~Card();
+ 
+        Ranks getRank();
+        Suits  getSuit();
+ 
+        virtual int getFaceValue();
+        
+    private:
+        Ranks RANK;
+        Suits SUIT;
+};
